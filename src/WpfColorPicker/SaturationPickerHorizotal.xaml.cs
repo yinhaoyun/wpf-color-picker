359024cec64d26427c76d027d1ffdcef7cde6be9
HY Yin <haoyun.tw@gmail.com>
2022-07-03 01:00:37 +0800

    Implement Horizontal Slider for LEET

---
+++ src/WpfColorPicker/SaturationPickerHorizotal.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Dsafa.WpfColorPicker
+{
+    /// <summary>
+    /// Interaction logic for SaturationPicker.xaml
+    /// </summary>
+    internal partial class SaturationPickerHorizontal : SliderPickerHorizontal
+    {
+        public static readonly DependencyProperty SaturationProperty
+            = DependencyProperty.Register(nameof(Saturation), typeof(double), typeof(SaturationPickerHorizontal), new PropertyMetadata(0.0, OnSaturationChanged));
+        public static readonly DependencyProperty HueProperty
+            = DependencyProperty.Register(nameof(Hue), typeof(double), typeof(SaturationPickerHorizontal), new PropertyMetadata(0.0, OnHueChanged));
+
+        public SaturationPickerHorizontal()
+        {
+            InitializeComponent();
+        }
+
+        public double Saturation
+        {
+            get => (double)GetValue(SaturationProperty);
+            set => SetValue(SaturationProperty, value);
+        }
+        public double Hue
+        {
+            get => (double)GetValue(HueProperty);
+            set => SetValue(HueProperty, value);
+        }
+
+        private static void OnSaturationChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var saturationPicker = (SaturationPickerHorizontal)o;
+            saturationPicker.UpdateAdorner((double)e.NewValue);
+        }
+        private static void OnHueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var saturationPicker = (SaturationPickerHorizontal)o;
+            saturationPicker.saturationGradients.GradientStops[0].Color = ColorHelper.FromHSV((double)e.NewValue, 1, 1);
+        }
+        private void UpdateAdorner(double saturation)
+        {
+            double percent = saturation;
+
+            // Make it so that the arrow doesn't jump back to the top when it goes to the bottom
+            //Point mousePos = Mouse.GetPosition(this);
+            //if (percent == 0 && ActualHeight - mousePos.Y < 1)
+            //{
+            //    percent = 1;
+            //}
+
+            AdornerVerticalPercent = percent;
+            AdornerColor = ColorHelper.FromHSV(Hue, saturation, 1);
+        }
+
+        protected override void OnAdornerPositionChanged(double verticalPercent)
+        {
+            Color c = saturationGradients.GradientStops.GetColorAtOffset(verticalPercent);
+            AdornerColor = c;
+            Saturation = c.GetSaturation();
+        }
+    }
+}
---
+++ src/WpfColorPicker/SliderPickerAdornerHorizontal.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Dsafa.WpfColorPicker
+{
+    internal class SliderPickerAdornerHorizontal : Adorner
+    {
+        private static readonly DependencyProperty VerticalPercentProperty 
+            = DependencyProperty.Register(nameof(VerticalPercent), typeof(double), typeof(SliderPickerAdornerHorizontal), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+        private static readonly DependencyProperty ColorProperty
+            = DependencyProperty.Register(nameof(Color), typeof(Color), typeof(SliderPickerAdornerHorizontal), new FrameworkPropertyMetadata(Colors.Red, FrameworkPropertyMetadataOptions.AffectsRender));
+        private static readonly Pen Pen = new Pen(Brushes.Black, 1);
+        private Brush _brush = Brushes.Red;
+
+        public SliderPickerAdornerHorizontal(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            IsHitTestVisible = false;
+        }
+
+        public double VerticalPercent
+        {
+            get => (double)GetValue(VerticalPercentProperty);
+            set => SetValue(VerticalPercentProperty, value);
+        }
+
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set
+            {
+                SetValue(ColorProperty, value);
+                _brush = new SolidColorBrush(value);
+            }
+        }
+
+        public Rect ElementSize { get; set; }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            var height = 13;
+            var x = ElementSize.Width * VerticalPercent;
+            var y = 6;
+
+            var triangleGeometry = new StreamGeometry();
+            using (var context = triangleGeometry.Open())
+            {
+                context.BeginFigure(new Point(x, y), true, true);
+                context.LineTo(new Point(x - 6, y + height), true, false);
+                context.LineTo(new Point(x + 6, y + height), true, false);
+            }
+
+            var transformGroup = new TransformGroup();
+            transformGroup.Children.Add(new ScaleTransform(-1, 1));
+            transformGroup.Children.Add(new TranslateTransform(ElementSize.Width, 0));
+            drawingContext.DrawGeometry(_brush, Pen, triangleGeometry);
+        }
+    }
+}
